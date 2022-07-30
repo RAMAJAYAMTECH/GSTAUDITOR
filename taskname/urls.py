2f620eb4ce1fa9dd87e6fcdcf6332981a0bac37a
RAMAJAYAMTECH <110171087+RAMAJAYAMTECH@users.noreply.github.com>
2022-07-30 15:59:03 +0530

    second

---
+++ taskname/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from todolist_app import views as todolist_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    #path('', todolist_views.index1a, name='index1a'),
+    #path('', todolist_views.indexapi, name='indexapi'),
+    #path('mul/', todolist_views.FileFieldView, name='FileFieldView'),
+    #path('2a', todolist_views.index2a, name='index2a'),
+    path('', todolist_views.gst, name='gst'),
+    ]
